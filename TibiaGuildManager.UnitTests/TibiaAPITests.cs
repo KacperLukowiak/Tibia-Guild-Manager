f0f260b3c047d76706f09730700e0a175d0ec628
KacperPC <kacperlukowiak@gmail.com>
2021-03-11 23:22:21 +0100

    Add project files.

---
+++ TibiaGuildManager.UnitTests/TibiaAPITests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using TibiaGuildManager.Data;
+using TibiaGuildManager.Services;
+
+namespace TibiaGuildManager.UnitTests
+{
+    public class TibiaAPITests
+    {
+        [Fact]
+        public async Task GetGuildTest()
+        {
+            TibiaAPIService tibiaAPIService = new TibiaAPIService();
+            var guild = await tibiaAPIService.GetGuild("Immortal");
+            Assert.NotNull(guild);
+        }
+    }
+}
---
+++ TibiaGuildManager/Services/TibiaAPIService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TibiaGuildManager.Data;
+
+namespace TibiaGuildManager.Services
+{
+    public class TibiaAPIService
+    {
+        private static HttpClient tibiaAPIClient;
+        private string APIURI;
+
+        public TibiaAPIService()
+        {
+            APIURI = "https://api.tibiadata.com/v2/";
+            tibiaAPIClient = new HttpClient();
+        }
+
+        public async Task<TibiaGuild.Root> GetGuild(string guild)
+        {
+            guild = guild.Replace(" ", "+");
+            HttpResponseMessage httpResponseMessage = await tibiaAPIClient.GetAsync(APIURI + "guild/" + guild + ".json");
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            string response = await httpResponseMessage.Content.ReadAsStringAsync();
+            TibiaGuild.Root tibiaGuild = JsonSerializer.Deserialize<TibiaGuild.Root>(response);
+            return tibiaGuild;
+        }
+    }
+}
